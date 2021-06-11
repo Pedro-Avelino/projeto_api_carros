8e59cb62fbe8305d67af192d1e6328505ce6978f
Pedro Avelino <pedroavelino@alu.uern.br>
2021-06-10 23:23:24 -0300

    Salvar carro

--- src/main/java/com/example/carros/api/CarrosController.java
+++ src/main/java/com/example/carros/api/CarrosController.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -32,4 +34,10 @@ public class CarrosController {
 		return service.getCarrosByTipo(tipo);
 	}
 	
+	@PostMapping
+	public String post(@RequestBody Carro carro) {
+		Carro c = service.save(carro);
+		
+		return "Carro salvo com sucesso: " + c.getId();
+	}	
 }
--- src/main/java/com/example/carros/domain/CarroService.java
+++ src/main/java/com/example/carros/domain/CarroService.java
@@ -1,9 +1,6 @@
 package com.example.carros.domain;
 
-import java.util.ArrayList;
-import java.util.List;
 import java.util.Optional;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -15,21 +12,11 @@ public class CarroService {
 	
 	public Iterable<Carro> getCarros() { return rep.findAll(); }
 	
-	public Optional<Carro> getCarroById(Long id) {
-		return rep.findById(id);
-	}
+	public Optional<Carro> getCarroById(Long id) {return rep.findById(id); }
 	
-	public Iterable<Carro> getCarrosByTipo(String tipo) {
-		return rep.findByTipo(tipo);
-	}
+	public Iterable<Carro> getCarrosByTipo(String tipo) { return rep.findByTipo(tipo); }
 	
-	public List<Carro> getCarrosfake() {
-		List<Carro> carros = new ArrayList<>();
-		
-		carros.add(new Carro(1L, "Fusca"));
-		carros.add(new Carro(2L, "Brasilia"));
-		carros.add(new Carro(3L, "Chevette"));
-		
-		return carros;
+	public Carro save(Carro carro) {
+		return rep.save(carro);
 	}
 }
