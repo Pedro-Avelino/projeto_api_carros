b4ea1e1b335f1c6c7cd8f332d48ae99cbf37dba1
Pedro Avelino <pedroavelino@alu.uern.br>
2021-06-11 13:25:00 -0300

    Atualizar carro

--- src/main/java/com/example/carros/domain/CarroService.java
+++ src/main/java/com/example/carros/domain/CarroService.java
@@ -3,6 +3,7 @@ package com.example.carros.domain;
 import java.util.Optional;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.Assert;
 
 @Service
 public class CarroService {
@@ -16,7 +17,30 @@ public class CarroService {
 	
 	public Iterable<Carro> getCarrosByTipo(String tipo) { return rep.findByTipo(tipo); }
 	
-	public Carro save(Carro carro) {
+	public Carro insert(Carro carro) {
+		Assert.isNull(carro.getId(), "Não foi possivel inserir o registro");
+		
 		return rep.save(carro);
+	}
+	
+	public Carro update(Carro carro, Long id) {
+		Assert.notNull(id, "Não foi possivel atualizar o registro");
+		
+		// Busca o carro no banco de dados
+		Optional<Carro> optional = getCarroById(id);
+		if(optional.isPresent()) {
+			Carro db = optional.get();
+			// Copiar as propriedades
+			db.setNome(carro.getNome());
+			db.setTipo(carro.getTipo());
+			System.out.println("Carro id " + db.getId());
+			
+			//Atualiza o carro
+			rep.save(db);
+			
+			return db;
+	} else {
+		throw new RuntimeException("Não foi possivel atualizar o registro");
+	}
 	}
 }
