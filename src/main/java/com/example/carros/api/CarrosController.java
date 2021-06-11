a578f3068ffe735449ac41e8208c79652555da05
Pedro Avelino <pedroavelino@alu.uern.br>
2021-06-10 22:47:56 -0300

    Projeto Criado

---
+++ src/main/java/com/example/carros/api/CarrosController.java
@@ -0,0 +1,21 @@
+package com.example.carros.api;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.example.carros.domain.Carro;
+import com.example.carros.domain.CarroService;
+
+@RestController
+@RequestMapping("/api/v1/carros")
+public class CarrosController {
+	@Autowired
+	private CarroService service;
+	
+	@GetMapping
+	public Iterable<Carro> get() {
+		return service.getCarros();
+	}
+}
